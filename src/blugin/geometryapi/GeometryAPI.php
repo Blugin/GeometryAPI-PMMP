8d13f32b6d9e0274d0529c685e781533f7ed0816
PresentKim <qjrmckd@gmail.com>
2018-05-02 20:24:30 +0900

    Merge CommandListener to main class

--- src/blugin/geometryapi/GeometryAPI.php
+++ src/blugin/geometryapi/GeometryAPI.php
@@ -2,13 +2,14 @@
 
 namespace blugin\geometryapi;
 
+use pocketmine\command\{
+  Command, PluginCommand, CommandExecutor, CommandSender
+};
 use pocketmine\plugin\PluginBase;
-use blugin\geometryapi\command\PoolCommand;
-use blugin\geometryapi\command\subcommands\ListSubCommand;
 use blugin\geometryapi\listener\PlayerEventListener;
 use blugin\geometryapi\lang\PluginLang;
 
-class GeometryAPI extends PluginBase{
+class GeometryAPI extends PluginBase implements CommandExecutor{
 
     /** @var GeometryAPI */
     private static $instance = null;
@@ -18,7 +19,7 @@ class GeometryAPI extends PluginBase{
         return self::$instance;
     }
 
-    /** @var PoolCommand */
+    /** @var PluginCommand */
     private $command;
 
     /** @var PluginLang */
@@ -35,28 +36,29 @@ class GeometryAPI extends PluginBase{
         if (!file_exists($dataFolder = $this->getDataFolder())) {
             mkdir($dataFolder, 0777, true);
         }
+        $this->language = new PluginLang($this);
+
+        if ($this->command !== null) {
+            $this->getServer()->getCommandMap()->unregister($this->command);
+        }
+        $this->command = new PluginCommand($this->language->translate('commands.geometry'), $this);
+        $this->command->setPermission('geometry.cmd');
+        $this->command->setDescription($this->language->translate('commands.geometry.description'));
+        $this->command->setUsage($this->language->translate('commands.geometry.usage'));
+        if (is_array($aliases = $this->language->getArray('commands.geometry.aliases'))) {
+            $this->command->setAliases($aliases);
+        }
+        $this->getServer()->getCommandMap()->register('geometryapi', $this->command);
+
         if (!file_exists($jsonFolder = "{$dataFolder}json/")) {
             mkdir($jsonFolder, 0777, true);
         }
-        $this->language = new PluginLang($this);
-
         $this->geometryDatas = [];
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($jsonFolder)) as $path => $fileInfo) {
             if (!is_dir($path) && strcasecmp(substr($path, -5), '.json') === 0) {
                 $this->geometryDatas[substr($fileName = $fileInfo->getFileName(), 0, strlen($fileName) - 5)] = file_get_contents($path);
             }
         }
-
-        if ($this->command == null) {
-            $this->command = new PoolCommand($this, 'geometry');
-            $this->command->createSubCommand(ListSubCommand::class);
-        }
-        $this->command->updateTranslation();
-        $this->command->updateSudCommandTranslation();
-        if ($this->command->isRegistered()) {
-            $this->getServer()->getCommandMap()->unregister($this->command);
-        }
-        $this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
 
         $this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
     }
@@ -68,10 +70,39 @@ class GeometryAPI extends PluginBase{
         if (!file_exists($jsonFolder = "{$dataFolder}json/")) {
             mkdir($jsonFolder, 0777, true);
         }
-
         foreach ($this->geometryDatas as $geometryName => $geometryData) {
             file_put_contents("{$jsonFolder}{$geometryName}.json", $geometryData);
         }
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param Command       $command
+     * @param string        $label
+     * @param string[]      $args
+     *
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        $list = array_keys($this->getGeometryDatas());
+        $max = ceil(count($list) / 5);
+        $page = 0;
+        if (isset($args[0]) && is_numeric($args[0])) {
+            $page = ((int) $args[0]) - 1;
+            if ($page < 0) {
+                $page = 0;
+            } elseif ($page > $max) {
+                $page = $max;
+            }
+        }
+        $sender->sendMessage($this->language->translate('commands.geometry.head', [
+          (string) ($page + 1),
+          (string) $max,
+        ]));
+        for ($i = $page * 5, $count = count($list), $loopMax = ($page + 1) * 5; $i < $count && $i < $loopMax; $i++) {
+            $sender->sendMessage($this->language->translate('commands.geometry.item ', [$list[$i]]));
+        }
+        return true;
     }
 
     /**  @return string[] */
@@ -101,9 +132,9 @@ class GeometryAPI extends PluginBase{
     /**
      * @param string $name = ''
      *
-     * @return PoolCommand
+     * @return PluginCommand
      */
-    public function getCommand(string $name = '') : PoolCommand{
+    public function getCommand(string $name = '') : PluginCommand{
         return $this->command;
     }
 
