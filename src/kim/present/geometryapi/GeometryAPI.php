651464b4d959874fd2a03e63f374b8bfbcad996c
PresentKim <qjrmckd@gmail.com>
2018-07-04 09:07:04 +0900

    Change visibility of onLoad(), onEnable() and onDisable() to protected
    According to https://github.com/pmmp/PocketMine-MP/commit/49bca0d5a1d18ab4adeee4c01276adf15e47600f
    According to https://github.com/pmmp/PocketMine-MP/pull/2268

--- src/kim/present/geometryapi/GeometryAPI.php
+++ src/kim/present/geometryapi/GeometryAPI.php
@@ -30,11 +30,17 @@ class GeometryAPI extends PluginBase implements CommandExecutor{
 	/** @var string[] */
 	private $geometryDatas = [];
 
-	public function onLoad() : void{
+	/**
+	 * Called when the plugin is loaded, before calling onEnable()
+	 */
+	protected function onLoad() : void{
 		self::$instance = $this;
 	}
 
-	public function onEnable() : void{
+	/**
+	 * Called when the plugin is enabled
+	 */
+	protected function onEnable() : void{
 		if(!file_exists($dataFolder = $this->getDataFolder())){
 			mkdir($dataFolder, 0777, true);
 		}
@@ -65,7 +71,11 @@ class GeometryAPI extends PluginBase implements CommandExecutor{
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener($this), $this);
 	}
 
-	public function onDisable() : void{
+	/**
+	 * Called when the plugin is disabled
+	 * Use this to free open things and finish actions
+	 */
+	protected function onDisable() : void{
 		if(!file_exists($dataFolder = $this->getDataFolder())){
 			mkdir($dataFolder, 0777, true);
 		}
